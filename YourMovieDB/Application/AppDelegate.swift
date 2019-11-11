942d04148025765ba76624dad0abde00bafe915e
tung-tx2038 <tran.xuan.tung@sun-asterik.com>
2019-11-11 10:03:03 +0700

    [task][18999]_home_screen

--- YourMovieDB/Application/AppDelegate.swift
+++ YourMovieDB/Application/AppDelegate.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import netfox
+import RxSwift
+import RxCocoa
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        configureNetfox()
+        bindViewModel()
         return true
+    }
+
+    private func bindViewModel() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else {
+            return
+        }
+        let viewModel = AppViewModel(navigator: AppNavigator(window: window))
+        let input = AppViewModel.Input(loadTrigger: Driver.just(()))
+        let output = viewModel.transform(input: input)
+        output.toMain
+            .drive()
+            .disposed(by: DisposeBag())
+    }
+    private func configureNetfox() {
+        NFX.sharedInstance().start()
     }
 }
---
+++ YourMovieDB/Extensions/UIView+Extension.swift
@@ -0,0 +1,102 @@
+//
+//  UIView+Extension.swift
+//  Common
+//
+//  Created by Tuấn Bờm on 5/4/18.
+//  Copyright © 2018 Tuấn Bờm. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        guard let view = Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)?.first as? T else {
+            return T()
+        }
+        return view
+    }
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            return layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            return layer.shadowColor = newValue?.cgColor
+        }
+    }
+
+    func setViewrounded() {
+        self.layer.cornerRadius = (self.frame.width / 2)
+        self.layer.masksToBounds = true
+    }
+}
+
+
+
